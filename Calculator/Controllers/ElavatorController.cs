363a6492a02d5fa599200555079c5a3263bbe8fb
AIilK <alirezailkhani84@gmail.com>
2024-12-19 14:53:14 +0330

    ins

---
+++ Calculator/Controllers/ElavatorController.cs
@@ -0,0 +1,62 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class ElavatorController : Controller
+    {
+        public IActionResult home()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            user.haghebimepaye1 = user.taohedp  * (user.nerkh / 100) * user.zarfiat ;
+            user.haghebimeh = user.haghebimepaye1 * (user.hazine / 100);
+            user.haghebimepaye2 = ((user.mahadi * 8 / 12) + (user.mahharam * 4 / 12)) * (user.nerkh / 1000) * (user.tonaz / 75);
+            user.haghebimepaye2 = user.haghebimepaye2 * (user.tedad / 100);
+            user.haghebimepaye2 = user.haghebimepaye2 * (user.afzayesh1 / 100);
+            user.haghebimepaye2 = user.haghebimepaye2 * (user.afzayesh2 / 100);
+            user.haghebimepaye2 = user.haghebimepaye2 * (user.afzayesh3 / 100);
+            user.haghebimepaye3 = user.maliha * (user.zaribe + 1) * (user.nerkh/100);
+            user.haghebimeh = user.haghebimepaye1 + user.haghebimepaye2 + user.haghebimepaye3;
+            user.haghebimeh = user.haghebimeh * (user.bedondar / 100);
+            user.haghebimeh = user.haghebimeh * (user.tejari / 100);
+            user.haghebimeh = user.haghebimeh * (user.bishaz / 100);
+            user.haghebimeh = user.haghebimeh * (user.bedonray / 100);
+            
+            return View(user);
+        }
+        [HttpGet]
+        public IActionResult nafarbar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult nafarbar(User user)
+        {
+            user.haghebimepaye1 = user.taohedp * (user.nerkh / 100) * user.zarfiat;
+            user.haghebimepaye2 = ((user.mahadi * 8 / 12) + (user.mahharam * 4 / 12)) * (user.nerkh / 1000) * (user.tonaz / 75);
+            user.haghebimeh = user.haghebimepaye1 + user.haghebimepaye2 + user.haghebimepaye3;
+            user.haghebimeh = user.haghebimeh * (user.bedondar / 100);
+            user.haghebimeh = user.haghebimeh * (user.tejari / 100);
+            user.haghebimeh = user.haghebimeh * (user.bishaz / 100);
+            user.haghebimeh = user.haghebimeh * (user.bedonray / 100);
+            user.haghebimeh = user.haghebimepaye2 * (user.tedad / 100);
+            user.haghebimeh = user.haghebimepaye2 * (user.afzayesh1 / 100);
+            user.haghebimeh = user.haghebimepaye2 * (user.afzayesh2 / 100);
+            user.haghebimeh = user.haghebimepaye2 * (user.afzayesh3 / 100);
+            user.haghebimeh = user.haghebimepaye1 * (user.hazine / 100);
+
+            return View(user);
+        }
+
+    }
+}
---
+++ Calculator/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "please enter the num")]
+        public long taohedp { get; set; }
+        [Required]
+        public long pezeshki { get; set; }
+        [Required]
+        public long mahadi { get; set; }
+        [Required]
+        public long mahharam { get; set; }
+        [Required]
+        public long maliha { get; set; }
+        [Required]
+        public long malitol { get; set; }
+        [Required]
+        public double zaribe { get; set; }
+        [Required]
+        public double nerkh { get; set; }
+        [Required]
+        public int zarfiat { get; set; }
+        [Required(ErrorMessage = "enter the num")]
+        [Display(Name = "تناژ")]
+        public double tonaz { get; set; }
+        
+       // public int masahat { get; set; }
+       // [Required]
+       // public int day { get; set; }
+       // [Required]
+       // public int hours { get; set; }
+
+
+
+       // elevators
+        public double haghebimepaye1 { get; set; }
+        public double haghebimepaye2 { get; set; }
+        public double haghebimepaye3 { get; set; }
+        public double haghebimeh { get; set; }
+        public double haghebimehmo { get; set; }
+        public double bedondar { get; set; }
+        public double tejari { get; set; }
+        public double bishaz { get; set; }
+        public double bedonray { get; set; }
+        public double teded { get; set; }
+        public double afzayesh1 { get; set; }
+        public double afzayesh2 { get; set; }
+        public double afzayesh3 { get; set; }
+        public double bimegozar { get; set; }
+        public double hazine { get; set; }
+        public double tedad { get; set; }
+        public double tasvie { get; set; }
+        public double adam { get; set; }
+        public double tashdid { get; set; }
+        public double tkhfifva { get; set; }
+        public double darsad { get; set; }
+
+
+    }
+}
